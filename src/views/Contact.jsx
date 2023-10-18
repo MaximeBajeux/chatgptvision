7a45b4d7b9e5cb270a43c2f44349559dc1f4189d
Maxime Bajeux <max.bajeux@gmail.com>
2023-10-18 11:05:22 +0200

    more minimalist

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -27,7 +27,7 @@ function ContactPage() {
             {error}
           </div>
         )}
-        <form name="contact" method="POST" netlify action="/merci">
+        <form name="contact" method="POST" netlify>
           <div className="mb-4">
             <label
               htmlFor="message"
